102ee648e045bb31a355e8de907f3b4d799429e7
Saikat Das <dassaikat93@yahoo.com>
2021-06-06 09:46:02 +0530

    added 1st hello world code

---
+++ GFG/Basics/1-hello-world.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+def print_fun():
+    
+    # Your code here
+    
+    print('Hello World')
+    
+    
+    # Please follow proper indentation#{ 
+#Driver Code Starts.
+
+def main():
+    print_fun()
+
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends

4828fa0babd0e270e783b3e43786a7972dedf09e
Saikat Das <dassaikat93@yahoo.com>
2021-06-06 10:39:34 +0530

    more code

---
+++ GFG/Basics/2-space-separated.py
@@ -0,0 +1,21 @@
+#u need to write each word separate by space without using space in string.
+#User function Template for python3
+
+# Function to print words with spaces
+def print_fun():
+    # Your code here
+    # Do not use space in the string
+    # try to use seperator
+    
+    print('Geeks','For','Geeks')
+    
+
+#{ 
+#Driver Code Starts.
+
+# Python Code to print Words with Space seperated
+def main():
+    print_fun()
+
+if __name__ == '__main__':
+    main()

6648a686d9d85aeca1dbb51778a05b6292644698
Saikat Das <dassaikat93@yahoo.com>
2021-07-03 20:59:12 +0530

    string slicing

---
+++ Hackerrank/15-python-mutation.py
@@ -0,0 +1,35 @@
+'''
+Read a given string, change the character at a given index and then print the modified string.
+Function Description
+
+Complete the mutate_string function in the editor below.
+
+mutate_string has the following parameters:
+
+string string: the string to change
+int position: the index to insert the character at
+string character: the character to insert
+Returns
+
+string: the altered string
+Sample Input
+
+STDIN           Function
+-----           --------
+abracadabra     s = 'abracadabra'
+5 k             position = 5, character = 'k'
+Sample Output
+
+abrackdabra
+'''
+def mutate_string(string, position, character):
+    
+    strPos = position + 1
+    newStr = string[:position] + character + string[strPos:]
+    return newStr
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)

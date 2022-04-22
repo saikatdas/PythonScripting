df01725254e4dbcf9c1e2845e368dd7f23931a1b
Saikat Das <sdas@cartrawler.com>
2022-04-22 11:40:16 +0100

    added comments

--- Hackerrank/23-list-comprehensions.py
+++ Hackerrank/23-list-comprehensions.py
@@ -1,3 +1,9 @@
+    """You are given three integers x,y,z and  representing the dimensions of a cuboid along with an integer n .
+    Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of i+j+k is not equal to n. Here, 0<=i<=x , 0<=i<=y , 0<=i<=z. Please use list comprehensions rather than multiple
+    loops, as a learning exercise.
+    """
+
+
 
 def createList(x):
     xList = []

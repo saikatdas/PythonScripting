5e82825329b986eff95e0ba915ff3b03f990334e
saikatdas <dassaikat93@yahoo.com>
2022-04-27 23:08:07 +0100

    input and output

--- Hackerrank/30-itertools-permutation.py
+++ Hackerrank/30-itertools-permutation.py
@@ -2,6 +2,40 @@
         for element in re:
             print(''.join(element))
 
+        >>> from itertools import permutations
+>>> print permutations(['1','2','3'])
+<itertools.permutations object at 0x02A45210>
+>>>
+>>> print list(permutations(['1','2','3']))
+[('1', '2', '3'), ('1', '3', '2'), ('2', '1', '3'), ('2', '3', '1'), ('3', '1', '2'), ('3', '2', '1')]
+>>>
+>>> print list(permutations(['1','2','3'],2))
+[('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2')]
+>>>
+>>> print list(permutations('abc',3))
+[('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
+
+Sample Input
+
+HACK 2
+Sample Output
+
+AC
+AH
+AK
+CA
+CH
+CK
+HA
+HC
+HK
+KA
+KC
+KH
+Explanation
+
+All possible size  permutations of the string "HACK" are printed in lexicographic sorted order.
+
     """
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT

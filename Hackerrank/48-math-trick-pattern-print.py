3acc38e2a6d741a4de10321a7038378f10675eed
saikatdas <dassaikat93@yahoo.com>
2022-05-11 14:03:47 +0100

    usual approch

--- Hackerrank/48-math-trick-pattern-print.py
+++ Hackerrank/48-math-trick-pattern-print.py
@@ -4,6 +4,11 @@ Math trick pattern print will not work if n<=14
 22
 333
 .......
+
+for i in range(1,99):
+    for j in range(1,i+1):
+        print(i, end='')
+    print()
 """
 for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
     print((10**(i)//9)*i)

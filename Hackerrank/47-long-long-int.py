1b8e1c7d54762d5f6f7bb83c78d3a797e1a00eb5
saikatdas <dassaikat93@yahoo.com>
2022-05-11 13:30:57 +0100

    big int addition

---
+++ Hackerrank/47-long-long-int.py
@@ -0,0 +1,10 @@
+"""
+Integers in Python can be as big as the bytes in your machine's memory. There is no limit in size as there is: 2^31 -1 (c++ int) or 2^63 -1 (C++ long long int)
+
+"""
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    print (pow(a,b) + pow(c,d))

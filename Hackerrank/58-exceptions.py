c4adacb4985f557b65459bae41af479e4a75eba8
saikatdas <dassaikat93@yahoo.com>
2022-05-22 17:32:02 +0100

    more docs

--- Hackerrank/58-exceptions.py
+++ Hackerrank/58-exceptions.py
@@ -1,6 +1,18 @@
 """
 try catch block
 
+Sample Input
+
+3
+1 0
+2 $
+3 1
+Sample Output
+
+Error Code: integer division or modulo by zero
+Error Code: invalid literal for int() with base 10: '$'
+3
+
 """
 if __name__ == '__main__':
     n = int(input())

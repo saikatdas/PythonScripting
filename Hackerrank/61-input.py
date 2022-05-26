53d174d13c91ef2587f737ca6359563fdbadf974
saikatdas <dassaikat93@yahoo.com>
2022-05-26 21:46:58 +0100

    docs

--- Hackerrank/61-input.py
+++ Hackerrank/61-input.py
@@ -1,3 +1,17 @@
+"""
+Sample Input
+
+1 4
+x**3 + x**2 + x + 1
+Sample Output
+
+True
+
+use eval : eval needs a variable named x to be defined
+If you don't have a variable named x , you will get an error
+
+"""
+
 if __name__ == '__main__':
     x,k=map(int, input().split())
     print (k==eval(input()))

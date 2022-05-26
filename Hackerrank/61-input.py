212ad31cec90f2572a7fb628797013fd4b997c21
saikatdas <dassaikat93@yahoo.com>
2022-05-26 21:41:13 +0100

    use eval

--- Hackerrank/61-input.py
+++ Hackerrank/61-input.py
@@ -1,24 +1,3 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import math
-def cal(p,t):
-    a = p.replace('x',str(t))
-    c = a.replace('**',',')
-    if (len(c) > 1):
-        d = math.pow(int(c[0]),int(c[2]))
-        return int(d)
-    return int(c)
 if __name__ == '__main__':
-    s = 0
-    sum_ = 0
-    x,y = map(int,input().split())
-    eq = input().split(' ')
-    for item in range(0,len(eq),1):
-
-        s = cal(eq[item],x)
-
-
-    if (sum_ == y):
-        print(sum_,True)
-    else:
-        print(sum_,False)
-        
+    x,k=map(int, input().split())
+    print (k==eval(input()))
